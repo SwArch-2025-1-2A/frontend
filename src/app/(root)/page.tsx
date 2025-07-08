683793042b86409cca0ec04c4e5f969258cd62ce
cesarALS <arthurolemos4@gmail.com>
2025-07-07 19:12:50 -0500

    refactor - remove some dangerous logs

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,9 +4,6 @@ import Hero from "../_components/landingPage/Hero";
 export default async function Home() {
   const hostname = process.env.HOSTNAME;
 
-  console.log("Environment Variables")
-  console.dir(process.env)
-
   return (
     <HydrateClient>
       <h2 style={{ color: "gray", fontSize: "14px" }}>
